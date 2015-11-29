b357121571fa3b8fa7faf2ecc417371268caf2f4
Jeffrey Jorgensen <jeff@jayjodesign.com>
2015-11-29 11:07:37 -0800

    first commit son

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# topdraw
-Top Draw Consulting
+#Top Draw Consulting
+===========
+
+Top Draw Consulting is a website for all your bad ass hunting needs.
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$body_class = 'home';
+$page_title = 'home';
+
+$jquery_plugins = array(
+	'something.js'
+	);
+
+include 'inc/header.php';
+
+?>
+
+<section id="content">
+
+</section>
+
+<?php include 'inc/footer.php'; ?>
